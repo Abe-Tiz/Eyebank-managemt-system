ffff3cefd961e24dee63a8a1fe4aacc19089efc6
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-01-27 10:00:07 +0300

    made some change

--- frontend/src/sections/donor/ViewDonor.js
+++ frontend/src/sections/donor/ViewDonor.js
@@ -44,25 +44,71 @@ const ViewDonor = () => {
   
   return (
     <div className="relative bg-white min-h-screen flex items-center justify-center">
-      <div className={`absolute top-8 left-64 w-52 h-52 bg-yellow-400 rounded-full opacity-60`}></div>
-      <div className={`absolute bottom-10 right-64 w-52 h-52 bg-orange-500 rounded-full opacity-60`} ></div>
+      <div
+        className={`absolute top-8 left-64 w-52 h-52 bg-yellow-400 rounded-full opacity-60`}
+      ></div>
+      <div
+        className={`absolute bottom-10 right-64 w-52 h-52 bg-orange-500 rounded-full opacity-60`}
+      ></div>
 
-      <div className="bg-gray-300 rounded-md overflow-hidden shadow-md p-6 mx-auto w-96 relative">
-        <div className={`absolute top-10 left-10 w-64 h-64 bg-indigo-400 rounded-full opacity-30`}  ></div>
-        <div className={`absolute -top-10 -right-10 w-${circleSize} h-${circleSize} bg-purple-600 rounded-full opacity-30`} ></div>
-        <div className={`absolute -bottom-10 -left-10 w-${circleSize} h-${circleSize} bg-pink-400 rounded-full opacity-30`}></div>
-        <div className={`absolute -bottom-10 -right-10 w-${circleSize} h-${circleSize} bg-blue-400 rounded-full opacity-30`}></div>
+      <div className="bg-gray-300 rounded-md overflow-hidden shadow-md p-6 mx-auto w-3/4 relative">
+        <div
+          className={`absolute top-10 left-10 w-64 h-64 bg-indigo-400 rounded-full opacity-30`}
+        ></div>
+        <div
+          className={`absolute -top-10 -right-10 w-${circleSize} h-${circleSize} bg-purple-600 rounded-full opacity-30`}
+        ></div>
+        <div
+          className={`absolute -bottom-10 -left-10 w-${circleSize} h-${circleSize} bg-pink-400 rounded-full opacity-30`}
+        ></div>
+        <div
+          className={`absolute -bottom-10 -right-10 w-${circleSize} h-${circleSize} bg-blue-400 rounded-full opacity-30`}
+        ></div>
 
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">{donorData.name} </h1>
-        <p className="text-gray-700 mb-2">Email: {donorData.email}</p>
-        <p className="text-gray-700 mb-2">City: {donorData.city}</p>
-        <p className="text-gray-700 mb-2">Age: {donorData.age}</p>
-        <p className="text-gray-700 mb-2">Sex: {donorData.sex}</p>
-        <p className="text-gray-700 mb-2">Subcity: {donorData.subcity}</p>
-        <p className="text-gray-700 mb-2">Kebele: {donorData.kebele}</p>
-        <p className="text-gray-700 mb-2">House Number: {donorData.HNumber}</p>
-        <p className="text-gray-700 mb-2">Mobile: {donorData.mobile}</p>
-       
+        <h2 className="text-4xl font-bold text-gray-800 mb-4">
+          Please check your information before print
+        </h2>
+        <div className="flex justify-center gap-x-80 ">
+          <div>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Name: <span>{donorData.name} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Email:{" "}
+              <span className="text-gray-700 mb-2">{donorData.email} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              City:{" "}
+              <span className="text-gray-700 mb-2">{donorData.city} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Age: <span className="text-gray-700 mb-2">{donorData.age} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Sex: <span className="text-gray-700 mb-2">{donorData.sex} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Subcity:{" "}
+              <span className="text-gray-700 mb-2">{donorData.subcity} </span>
+            </p>
+          </div>
+
+          <div>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Kebele:{" "}
+              <span className="text-gray-700 mb-2">{donorData.kebele} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              House Number:{" "}
+              <span className="text-gray-700 mb-2">{donorData.HNumber} </span>
+            </p>
+            <p className="text-xl font-bold text-gray-800 mb-2">
+              Mobile:{" "}
+              <span className="text-gray-700 mb-2">{donorData.mobile} </span>
+            </p>
+          </div>
+        </div>
+
         <div className="flex justify-between mt-6 ">
           <Link
             className="like-button bg-transparent text-black cursor-pointer px-6 py-3 rounded focus:outline-none border border-gray-300 hover:bg-blue-600 shadow-md z-30"
@@ -79,7 +125,6 @@ const ViewDonor = () => {
             Print
           </Link>
         </div>
-    
       </div>
     </div>
     // </div>
