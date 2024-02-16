89fef396e6630b1bf54328000cd430fc9af2aa7b
todayAwoke <awokedejenie@gmail.com>
2024-02-16 13:25:34 +0300

    lab technical dashboard

---
+++ frontend/src/components/labTechnical/DonorRegistration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DonorRegistration = () => {
+    return (
+        <div>DonorRegistration</div>
+    )
+}
+
+export default DonorRegistration
---
+++ frontend/src/components/labTechnical/ScreenDonor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScreenDonor = () => {
+    return (
+        <div>ScreenDonor</div>
+    )
+}
+
+export default ScreenDonor
