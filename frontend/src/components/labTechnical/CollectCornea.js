b04086ef6b03a6eeac917af3bef99d53a6285a43
todayAwoke <awokedejenie@gmail.com>
2024-03-05 20:45:43 +0300

    add cornea

--- frontend/src/components/labTechnical/CollectCornea.js
+++ frontend/src/components/labTechnical/CollectCornea.js
@@ -2,46 +2,40 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+
 import "../../static/styles/cornea.css";
 import { useTranslation } from "react-i18next";
 import { useToast } from "@chakra-ui/react";
 const CollectCornea = () => {
-    const [serNo, setSerNo] = useState('');
+
     const [dateOfRecovery, setDateOfRecovery] = useState('');
-    const [recoverySite, setRecoverySite] = useState('');
     const [recoveryTechnical, setRecoveryTechnical] = useState('');
-    const [serologyTest, setSerologyTest] = useState('');
-    const [covid, setCovid] = useState('');
-    const [corneaEvaluation, setCorneaEvaluation] = useState('');
-    const [distributionDate, setDistributionDate] = useState('');
-    const [surgeonName, setSurgeonName] = useState('');
-    const [surgeonType, setSurgeonType] = useState('');
-    const [hospitalStatus, setHospitalStatus] = useState('');
-    const [deliveryBy, setDeliveryBy] = useState('');
-    const [transportationMode, setTransportationMode] = useState('');
-    const [remark, setRemark] = useState('');
+    const [position, setPosition] = useState('');
+    const [eyeLid, setEyeLid] = useState('');
+    const [size, setSize] = useState('');
+    const [irisColor, setIrisColor] = useState('');
+    const [corneaStatus, setCorneaStatus] = useState('');
+    const [clarity, setClarity] = useState('');
+    const [lens, setLens] = useState('');
+    //for recovery
     const navigate = useNavigate();
     const toast = useToast();
     const { t } = useTranslation();
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         const data = {
-            serNo,
-            dateOfRecovery,
-            recoverySite,
-            recoveryTechnical,
-            serologyTest,
-            covid,
-            corneaEvaluation,
-            distributionDate,
-            surgeonName,
-            surgeonType,
-            hospitalStatus,
-            deliveryBy,
-            transportationMode,
-            remark
-        }
 
+            dateOfRecovery,
+            position,
+            eyeLid,
+            size,
+            irisColor,
+            corneaStatus,
+            clarity,
+            lens,
+            recoveryTechnical,
+        }
+        console.log(data);
         try {
             const response = await axios.post('http://localhost:4000/cornea/create', data);
             console.log(response.data);
@@ -62,166 +56,147 @@ const CollectCornea = () => {
     const handleDateOfRecoveryChange = (e) => {
         const inputValue = e.target.value;
         const modifiedValue = `ET${inputValue}`;
-
         setDateOfRecovery(modifiedValue);
     };
-    const handleRecoverySite = (e) => {
-        const data = e.target.value;
-        if (!data) {
-            alert("Please select a recovery site");
-        } else {
-            setRecoverySite(e.target.value);
-        }
-    }
+    const handlePosition = (event) => {
+        setPosition(event.target.value);
+    };
     return (
         <div>
-            <h2 className="header" style={{ textAlign: 'center' }}>Welcome to Collect Cornea Form</h2>
+            <h2 className="header" style={{ textAlign: 'center' }}>Welcome to Cornea Recovery Form</h2>
             <form onSubmit={handleFormSubmit}>
-            
-                    <div className="grid grid-cols-2">
-                        <label>
-                            Date of Recovery:
-                            <input
-                                type="date"
-                                value={dateOfRecovery}
-                                onChange={handleDateOfRecoveryChange}
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                            />
-                        </label>
-                        <label>
-                            Recovery Site:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={recoverySite} onChange={handleRecoverySite}>
-                                <option value="">Select Recovery Site</option>
-                                <option value="Site 1">Site 1</option>
-                                <option value="Site 2">Site 2</option>
-                                <option value="Site 3">Site 3</option>
-
-                            </select>
-                        </label>
-                        <label>
-                            Recovery Technical:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={recoveryTechnical}
-                                onChange={(e) => setRecoveryTechnical(e.target.value)}
-                            >
-                                <option value="">Select Recovery Technical</option>
-                                <option value="Technical 1">Technical 1</option>
-                                <option value="Technical 2">Technical 2</option>
-                                <option value="Technical 3">Technical 3</option>
-                            </select>
-                        </label>
-                        <label>
-                            Serology Test:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={serologyTest} onChange={(e) => setSerologyTest(e.target.value)}>
-                                <option value="">Select Serology Test</option>
-                                <option value="Test 1">Test 1</option>
-                                <option value="Test 2">Test 2</option>
-                                <option value="Test 3">Test 3</option>
-                            </select>
-                        </label>
-                        <label>
-                            Covid:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={covid} onChange={(e) => setCovid(e.target.value)}>
-                                <option value="">Select Covid</option>
-                                <option value="Yes">Yes</option>
-                                <option value="No">No</option>
-                            </select>
-                        </label>
-                        <label>
-                            Cornea Evaluation:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={corneaEvaluation}
-                                onChange={(e) => setCorneaEvaluation(e.target.value)}
-                            >
-                                <option value="">Select Cornea Evaluation</option>
-                                <option value="Evaluation 1">Evaluation 1</option>
-                                <option value="Evaluation 2">Evaluation 2</option>
-                                <option value="Evaluation 3">Evaluation 3</option>
-                            </select>
-                        </label>
-                        <label>
-                            Distribution Date:
-                            <input
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                type="date"
-                                value={distributionDate}
-                                onChange={(e) => setDistributionDate(e.target.value)}
-                            />
-                        </label>
-                    </div>
-                    <div className="grid grid-cols-2">
-                        <label>
-                            Name of Surgeon:
-                            <input
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                type="text"
-                                value={surgeonName}
-                                onChange={(e) => setSurgeonName(e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            Type of Surgeon:
-                            <input
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                type="text"
-                                value={surgeonType}
-                                onChange={(e) => setSurgeonType(e.target.value)}
-                            />
-                        </label>
-                        <label>
-                            Hospital Status:
-                            <select
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={hospitalStatus} onChange={(e) => setHospitalStatus(e.target.value)}>
-                                <option value="">Select Hospital Status</option>
-                                <option value="Status 1">Status 1</option>
-                                <option value="Status 2">Status 2</option>
-                                <option value="Status 3">Status 3</option>
-                            </select>
-                        </label>
-                        <label>
-                            Delivery By:
-                            <input
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                type="text" value={deliveryBy} onChange={(e) => setDeliveryBy(e.target.value)} />
-                        </label>
-                        <label>
-                            Mode of Transportation:
-                            <input
-                                className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                type="text"
-                                value={transportationMode}
-                                onChange={(e) => setTransportationMode(e.target.value)}
-                            />
-                        </label>
-                        <label >
-                            Remark:
-                            <textarea
-                                className="form-textarea mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                                value={remark}
-                                placeholder="Enter Remark"
-                                onChange={(e) => setRemark(e.target.value)}
-                            />
-                        </label>
-                    </div>
-
-                    <div className="mt-6">
-                        <button
-                            onClick={handleFormSubmit}
-                            type="submit"
-                            className="w-1/3 mr-4 py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded"
+                <div className="grid grid-cols-2">
+                    <label>
+                        Date of Recovery:
+                        <input
+                            type="date"
+                            value={dateOfRecovery}
+                            onChange={handleDateOfRecoveryChange}
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                        />
+                    </label>
+                    <label>
+                        Recovery Technical:
+                        <select
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            value={recoveryTechnical}
+                            onChange={(e) => setRecoveryTechnical(e.target.value)}
                         >
-                            Add Cornea
-                        </button>
+                            <option value="">Select Recovery Technical</option>
+                            <option value="Technical 1">Technical 1</option>
+                            <option value="Technical 2">Technical 2</option>
+                            <option value="Technical 3">Technical 3</option>
+                        </select>
+                    </label>
+                    <div>
+                        <label>
+                            <input
+                                type="radio"
+                                value="left"
+                                checked={position === 'left'}
+                                onChange={handlePosition}
+
+                            />
+                            Left
+                        </label>
+
+                        <label>
+                            <input
+                                type="radio"
+                                value="right"
+                                checked={position === 'right'}
+                                onChange={handlePosition}
+                            />
+                            Right
+                        </label>
                     </div>
+
+                    <label>
+                        iris Color
+                        <select
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            value={irisColor} onChange={(e) => setIrisColor(e.target.value)}>
+                            <option value="">Select Iris Color</option>
+                            <option value="Yes">blue</option>
+                            <option value="No">black</option>
+                        </select>
+                    </label>
+                    <label>
+                        Lens:
+                        <select
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            value={lens}
+                            onChange={(e) => setLens(e.target.value)}
+                        >
+                            <option value="">Select Cornea Evaluation</option>
+                            <option value="Evaluation 1">lens 1</option>
+                            <option value="Evaluation 2">Evaluation 2</option>
+                            <option value="Evaluation 3">Evaluation 3</option>
+                        </select>
+                    </label>
+                    <label>
+                        clarity:
+                        <select
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            value={clarity}
+                            onChange={(e) => setClarity(e.target.value)}
+                        >
+                            <option value="">Select Clarity</option>
+                            <option value="Evaluation 1">Clarity 1</option>
+                            <option value="Evaluation 2">Clarity 2</option>
+                            <option value="Evaluation 3">Clarity 3</option>
+                        </select>
+
+                    </label>
+                </div>
+                <div className="grid grid-cols-2">
+
+
+                    <label>
+                        corneaS Status
+                        <select
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            value={corneaStatus}
+                            onChange={(e) => setCorneaStatus(e.target.value)}>
+                            <option value="">Select Cornea Status</option>
+                            <option value="Status 1">Status 1</option>
+                            <option value="Status 2">Status 2</option>
+                            <option value="Status 3">Status 3</option>
+                        </select>
+                    </label>
+                    <label>
+                        size:
+                        <input
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            type="number"
+                            value={size}
+                            onChange={(e) => setSize(e.target.value)}
+                            min={0}
+                            max={100}
+                            step={1}
+                        />
+
+                    </label>
+                    <label>
+                        eyeLid:
+                        <input
+                            className="form-input mt-1 block w-4/5 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                            type="text"
+                            value={eyeLid}
+                            onChange={(e) => setEyeLid(e.target.value)}
+                        />
+
+                    </label>
+                </div>
+                <div className="mt-6">
+                    <button
+                        // onClick={handleFormSubmit}
+                        type="submit"
+                        className="w-1/3 mr-4 py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded"
+                    >
+                        Add Cornea
+                    </button>
+                </div>
             </form >
         </div >
     );
--- frontend/src/components/labTechnical/RecordMedicalParticular.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RecordMedicalParticular = () => {
-  return (
-    <div>RecordMedicalParticular</div>
-  )
-}
-
-export default RecordMedicalParticular
