c4d0abf9508312afb3d456fc8e975a562087d9a3
t21ttt <127199554+t21ttt@users.noreply.github.com>
2024-03-19 08:51:03 +0300

    updatting the model by adding the auther

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const {Schema,model} = mongoose;
 
 const PostSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
   title: {
     type: String,
     required: true,
