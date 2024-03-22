2f5d651d8b570bb17c60b06ef9e6ca7a8ecc9693
todayAwoke <awokedejenie@gmail.com>
2024-03-22 17:12:31 +0300

    add

--- backend/routes/contactRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { CreateContact } = require('../controller/ContactController');
-const router = express.Router();
-
-router.post('/create', CreateContact);
-module.exports = router;
