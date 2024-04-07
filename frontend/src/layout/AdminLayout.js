d470981d953c623cdf616e3871d2946b47a312d9
todayAwoke <awokedejenie@gmail.com>
2024-04-07 23:21:56 +0300

    add button color

--- frontend/src/layout/AdminLayout.js
+++ frontend/src/layout/AdminLayout.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import CustomSidebar from "./../components/CustomeSider";
- 
+
 import socketIOClient from "socket.io-client";
 import HeaderComponent from "../pages/dashboard/admins/HeaderComponent";
 
@@ -14,134 +14,132 @@ const socket = socketIOClient(ENDPOINT);
 const { Content } = Layout;
 
 const AdminDashboard = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [state, setState] = useState({
-    name: "",
-    image: "",
-    isDropdownOpen: false,
-    isLoggedin: false,
-    collapsed: false,
-    role: "",
-  });
-
-  const [reportData, setReportData] = useState({
-    donor: "",
-    user: "",
-  });
-
-  const [newDonorCount, setNewDonorCount] = useState(0);
-
-  const getLoggedInUser = async () => {
-    fetch("http://127.0.0.1:4000/user/userLogedin", {
-      method: "POST",
-      crossDomain: true,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        token: localStorage.getItem("token"),
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data, "user logged in");
-        setState((prev) => ({
-          ...prev,
-          name: data.data.name,
-          image: data.data.image,
-          role: data.data.role,
-          isLoggedin: true,
-        }));
-
-        if (data.data === "token expired") {
-          localStorage.clear();
-          navigate("/login");
-        }
-      });
-  };
-
-  const numberDonor = async () => {
-    try {
-      const response = await axios.get("http://localhost:4000/report");
-
-      setReportData((prevReportData) => ({
-        ...prevReportData,
-        donor: response.data,
-      }));
-
-      console.log(response.data);
-    } catch (error) {
-      console.log("Error : ", error);
-    }
-  };
-
-  // get notification when new donor registers
-  const newDOnorRetrive = () => {
-    socket.on("newDonorNotification", (data) => {
-      console.log("data : ", data.count);
-      setNewDonorCount(data.count);
+    const [state, setState] = useState({
+        name: "",
+        image: "",
+        isDropdownOpen: false,
+        isLoggedin: false,
+        collapsed: false,
+        role: "",
     });
 
-    return () => {
-      socket.off("newDonorNotification");
+    const [reportData, setReportData] = useState({
+        donor: "",
+        user: "",
+    });
+
+    const [newDonorCount, setNewDonorCount] = useState(0);
+
+    const getLoggedInUser = async () => {
+        fetch("http://127.0.0.1:4000/user/userLogedin", {
+            method: "POST",
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                token: localStorage.getItem("token"),
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.data, "user logged in");
+                setState((prev) => ({
+                    ...prev,
+                    name: data.data.name,
+                    image: data.data.image,
+                    role: data.data.role,
+                    isLoggedin: true,
+                }));
+
+                if (data.data === "token expired") {
+                    localStorage.clear();
+                    navigate("/login");
+                }
+            });
     };
-  };
 
-  useEffect(() => {
-    numberDonor();
-    getLoggedInUser();
-    newDOnorRetrive();
-  }, [setReportData, navigate, newDonorCount]);
+    const numberDonor = async () => {
+        try {
+            const response = await axios.get("http://localhost:4000/report");
+            setReportData((prevReportData) => ({
+                ...prevReportData,
+                donor: response.data,
+            }));
 
-  const handleSearchInputChange = (e) => {
-    // setSearchText(e.target.value);
-  };
+            console.log(response.data);
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    };
 
-  const toggleDropdown = () => {
-    setState({ ...state, isDropdownOpen: !state.isDropdownOpen });
-  };
+    // get notification when new donor registers
+    const newDOnorRetrive = () => {
+        socket.on("newDonorNotification", (data) => {
+            console.log("data : ", data.count);
+            setNewDonorCount(data.count);
+        });
 
-  const toggleSidebar = () => {
-    setState((prev) => ({ ...prev, collapsed: !prev.collapsed }));
-  };
+        return () => {
+            socket.off("newDonorNotification");
+        };
+    };
 
-  const [notifications, setNotifications] = useState([]);
+    useEffect(() => {
+        numberDonor();
+        getLoggedInUser();
+        newDOnorRetrive();
+    }, [setReportData, navigate, newDonorCount]);
 
-  return (
-    <Layout className=" bg-base-200 min-h-screen w-full grid  md:grid-cols-1 ">
-      {/* side bar section */}
-      <CustomSidebar
-        collapsed={state.collapsed}
-        toggleSidebar={toggleSidebar}
-        name={state.name}
-        image={state.image}
-        role={state.role}
-      />
+    const handleSearchInputChange = (e) => {
+        // setSearchText(e.target.value);
+    };
 
-      <Layout
-        className={`${
-          state.collapsed ? "ml-20" : "ml-64"
-        }  bg-base-200 transition-all duration-300 ease-in-out flex-grow`}
-      >
-        {/* header componnet  */}
-        <HeaderComponent
-          state={state}
-          toggleSidebar={toggleSidebar}
-          newDonorCount={newDonorCount}
-        />
+    const toggleDropdown = () => {
+        setState({ ...state, isDropdownOpen: !state.isDropdownOpen });
+    };
 
-        {/* content section  */}
-        <Content className="p-4 mt-10">
-          <div className=" bg-base-200  w-full">
-            <Outlet />
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
-  );
+    const toggleSidebar = () => {
+        setState((prev) => ({ ...prev, collapsed: !prev.collapsed }));
+    };
+
+    const [notifications, setNotifications] = useState([]);
+
+    return (
+        <Layout className=" bg-base-200 min-h-screen w-full grid  md:grid-cols-1 ">
+            {/* side bar section */}
+            <CustomSidebar
+                collapsed={state.collapsed}
+                toggleSidebar={toggleSidebar}
+                name={state.name}
+                image={state.image}
+                role={state.role}
+            />
+
+            <Layout
+                className={`${state.collapsed ? "ml-20" : "ml-64"
+                    }  bg-base-200 transition-all duration-300 ease-in-out flex-grow`}
+            >
+                {/* header componnet  */}
+                <HeaderComponent
+                    state={state}
+                    toggleSidebar={toggleSidebar}
+                    newDonorCount={newDonorCount}
+                />
+
+                {/* content section  */}
+                <Content className="p-4 mt-10">
+                    <div className=" bg-base-200  w-full">
+                        <Outlet />
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default AdminDashboard;
