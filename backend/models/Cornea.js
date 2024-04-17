f8b38c37ced5dea91fdbd81c9b2367ae019dab51
todayAwoke <awokedejenie@gmail.com>
2024-04-18 00:27:28 +0300

    add the feature

--- backend/models/Cornea.js
+++ backend/models/Cornea.js
@@ -3,89 +3,90 @@ var currentDate = new Date();
 var expiration = new Date(currentDate.getTime() + (14 * 24 * 60 * 60 * 1000));
 var expirationDate = expiration.toISOString();
 const corneaSchema = new mongoose.Schema(
-  {
-    examId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "PhysicalExam",
+    {
+        examId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "PhysicalExam",
+        },
+        lotNo: {
+            type: String,
+        },
+        dateOfRecovery: {
+            type: Date,
+            default: Date.now,
+        },
+        recoveryTechnical: {
+            type: String,
+        },
+        position: {
+            type: String,
+            enum: ["left", "right"],
+        },
+        eyeLid: {
+            type: String,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        irisColor: {
+            type: String,
+        },
+        corneaStatus: {
+            type: String,
+            // default: false
+        },
+        clarity: {
+            type: String,
+        },
+        lens: {
+            type: String,
+        },
+        distributed: {
+            type: Boolean,
+            default: false
+        },
+        expirationDate: {
+            type: Number,
+            default: 0,
+        },
+        // expirationDate: {
+        //     type: Date,
+        //     default: expirationDate
+        // },
+        evaluation: {
+            evaluationDate: {
+                type: Date,
+            },
+            epitheliam: {
+                type: String,
+            },
+            stroma: {
+                type: String,
+            },
+            endothelium: {
+                type: String,
+            },
+            evaluater: {
+                type: String,
+            },
+            evaluationComment: {
+                type: String,
+            },
+            approval: {
+                type: String,
+                // enum: ['yes', 'no']
+            },
+            suiatablity: {
+                type: String,
+                // enum: ['PK', 'EK', 'ALK', 'KLA', 'K-Pro', 'Therapeutic']
+            },
+            reason: {
+                type: String,
+                // enum: ['epitheliam', 'stroma', 'endothelium', 'descement', 'other']
+            },
+        },
     },
-    lotNo: {
-      type: String,
-    },
-    dateOfRecovery: {
-      type: Date,
-      default: Date.now,
-    },
-    recoveryTechnical: {
-      type: String,
-    },
-    position: {
-      type: String,
-      enum: ["left", "right"],
-    },
-    eyeLid: {
-      type: String,
-    },
-    size: {
-      type: Number,
-    },
-    irisColor: {
-      type: String,
-    },
-    corneaStatus: {
-      type: String,
-      // default: false
-    },
-    clarity: {
-      type: String,
-    },
-    lens: {
-      type: String,
-    },
-    distributed: {
-        type: Boolean,
-        default: false
-    },
-    expirationDate: {
-      type: Number,
-      default: 0,
-    },
-    // expirationDate: {
-    //     type: Date,
-    //     default: expirationDate
-    // },
-    evaluation: {
-      evaluationDate: {
-        type: Date,
-      },
-      epitheliam: {
-        type: String,
-      },
-      stroma: {
-        type: String,
-      },
-      endothelium: {
-        type: String,
-      },
-      evaluater: {
-        type: String,
-      },
-      evaluationComment: {
-        type: String,
-      },
-      approval: {
-        type: String,
-        // enum: ['yes', 'no']
-      },
-      suiatablity: {
-        type: String,
-        // enum: ['PK', 'EK', 'ALK', 'KLA', 'K-Pro', 'Therapeutic']
-      },
-      reason: {
-        type: String,
-        // enum: ['epitheliam', 'stroma', 'endothelium', 'descement', 'other']
-      },
-    },
-  },
 
     { timestamps: true }
 );
